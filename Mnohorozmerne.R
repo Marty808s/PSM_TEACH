8be2e029131e4e51e4b583443b65213e551bbcd3
J-Viola <honza.viola@email.cz>
2024-08-26 19:08:13 +0200

    update

--- Mnohorozmerne.R
+++ Mnohorozmerne.R
@@ -115,3 +115,203 @@ plot(PC)
 
 ## Faktorova analyza
 
+factanal(m1, factors = 3)
+# faktorova analyza jen prerotuje hlavni komponenty
+#	metoda rotace 'varimax' je brana jako zakladni (defaultni) 
+# vypis loadingu a procent vysvetlene variability
+(sc<-factanal(~v1+v2+v3+v4+v5+v6, factors = 3,scores = "Bartlett")$scores)
+sc
+plot(sc[,1],sc[,2],pch=19, main="Prvni 2 faktory")
+# vykresleni prvnich dvou faktoru
+################################
+## Samostatne
+# Nacteni dat Ichs.RData
+
+# Zavisi biologicke ukazatele (vaha, vyska a oba tlaky) na vzdelani a koureni?
+#   promenne vyska, hmot, syst, diast, Vzdel a Koureni
+
+# Urcete hlavni komponenty a faktory pro promenne vyska, hmot, syst, diast a vek
+
+# V datovem souboru mtcars sledujte zavislost vykonostnich charakteristik
+#   (mpg, disp, hp, drat, wt, qsec) na ostatnich promennych
+
+# Kolik hlavnich komponent/ faktoru je potreba pro reprezentaci vykonostnich promennych?
+#   A jake to jsou?
+
+################################
+# Zavisi biologicke ukazatele (vaha, vyska a oba tlaky) na vzdelani a koureni?
+#   promenne vyska, hmot, syst, diast, Vzdel a Koureni
+
+data <- Ichs
+
+vyska <- data$vyska
+hmotnost <- data$hmot
+syst <- data$syst
+dias <- data$diast
+
+vzdelani <- factor(data$Vzdel)
+koureni <- factor(data$Koureni)
+
+dataaov <- cbind(vyska, hmotnost, syst, dias)
+
+model <- manova(dataaov ~ vzdelani * koureni)
+summary.aov(model)
+
+# Z analýzy rozptylu vidíme, že tlak (syst a dias) je ovlivněn kouřením
+# P values: 3.364e-05 *** | syst, 0.0001267 *** | dias
+# Dále výška je ovlivněna vzděláním (pravdpěodobně ZŠ/SŠ/VŠ -> věkový rozdíl)
+# P value: 0.02367 *
+################################
+# Urcete hlavni komponenty a faktory pro promenne vyska, hmot, syst, diast a vek
+vyska <- data$vyska
+hmotnost <- data$hmot
+syst <- data$syst
+dias <- data$diast
+vek <- data$vek
+
+
+data2 <- cbind(vyska, hmotnost, syst, dias, vek)
+cor(data2)
+eigen(cor(data2))
+
+screeplot(princomp(data2),type="l")
+abline(h=1,col="green")
+
+prcomp(data2)
+cumsum(eigen(cor(data2))$values/sum(eigen(cor(data2))$values))
+# [1] 0.4064445 0.6748415 0.8675851 0.9598314 1.0000000
+factanal(data2, factors=2)
+# Hlavní komponenty jsou tlak (syst + dias) a tělesná stavba (hmotnost + vyska)
+#         Factor1 Factor2
+#vyska             0.667 
+#hmotnost  0.198   0.761 
+#syst      0.981   0.121 
+#dias      0.797   0.121 
+#vek              -0.135 
+#Factor1 Factor2
+#SS loadings      1.641   1.072
+#Proportion Var   0.328   0.214
+#Cumulative Var   0.328   0.543
+
+# Faktorová analýza nám vysvětluje 54.3% variability (není úplně dobrý model? - málo faktorů)
+################################
+# V datovem souboru mtcars sledujte zavislost vykonostnich charakteristik
+#   (mpg, disp, hp, drat, wt, qsec) na ostatnich promennych
+cars <- mtcars
+
+mpg <- cars$mpg
+disp <- cars$disp
+hp <- cars$hp
+drat <- cars$drat
+wt <- cars$wt
+qsec <- cars$qsec
+
+cars2 <- cbind(mpg, disp, hp, drat, wt, qsec)
+cars3 <- cbind(cars$cyl, cars$vs, cars$am, cars$gear, cars$carb)
+
+model <- manova(cars2 ~ cars$cyl * cars$vs * cars$am * cars$gear * cars$carb)
+
+summary.aov(model)
+# Závislost výkonostních charakteristik je značná hlavně u proměnných cyl, vs, am
+# P values: 1.648e-06 *** | cyl, 4.477e-05 *** | vs, 3.252e-05 *** | am
+################################
+# Kolik hlavnich komponent/ faktoru je potreba pro reprezentaci vykonostnich promennych?
+#   A jake to jsou?
+cor(cars2)
+eigen(cor(cars2))
+
+
+screeplot(princomp(cars2),type="l")
+abline(h=1,col="green")
+
+prcomp(cars2)
+cumsum(eigen(cor(cars2))$values/sum(eigen(cor(cars2))$values))
+# [1] 0.6978994 0.8892514 0.9448109 0.9705376 0.9913370 1.0000000
+factanal(cars2, factors=2)
+# Pro reprezentaci výkonnostních proměnných jsme určili tyto 2 faktory:
+# Výkonnost (disp + hp + wt)
+# Rychlost (qsec + mpg)
+################################
+
+## Kanonicka korelace
+#	korelace mezi dvema skupinami promennych
+# pracujme s charakteristikami statu: osobni uspory, podil populace do 15 let,
+#	podil populace nad 75 let, prijem na obyvatele, narust prijmu na obyvatele
+# rozdelime promenne do skupin: populacni podily, ekonomicke charakteristiky
+pop <- LifeCycleSavings[, 2:3]
+oec <- LifeCycleSavings[, -(2:3)]
+cancor(pop, oec)
+# TODO
+# vypocet kanonickych korelaci
+#	na vystupu jsou kanonicke korelace (jejich pocet je stejny jako 
+#	pocet promennych v mensi skupine), koeficienty kanonickych promennych
+#	prumery promennych
+
+
+################################
+## Diskriminační analýza
+library(MASS)
+# databaze o trech druzich kosatcu: Setosa (s), Versicolour (c), Virginica (v)
+# mereny jsou 4 ukazatele: sepal length & width, petal length & width
+#	kalisni a okvetni listek, vzdy delka a sirka
+Iris <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),Sp = rep(c("s","c","v"), rep(50,3)))
+
+# nahodny vyber 75 rostlin z cele databaze
+train <- sample(1:150, 75) # 75 náhodných čísel v intervalu 1-150
+table(Iris$Sp[train])
+#tvorba trénovacích dat
+
+# vstupni data do diskriminacni analyzy ... rostliny, u nichz presne zname druh
+(z <- lda(Sp ~ ., Iris, prior = c(1,1,1)/3, subset = train))
+# linearni diskriminacni analyza
+# vystup: vstupni (apriori) PRAVDĚPODOBNOSTI ... jake je ocekavane zastoupeni skupin v populaci 
+#	prumery promennych ve skupinach a koeficienty linearnich diskriminacnich funkci
+
+predpovedi<-predict(z, Iris[-train, ])
+predpovedi$x
+# vysledne hodnoty diskriminacnich funkci
+predpovedi$posterior
+# pravdepodobnosti zarazeni do jednotlivych populaci
+predpovedi$class
+# na zaklade vytvorene klasifikacni funkce priradi nova mereni do skupin
+#	vybere idealni skupinu + vypocte pravdepodobnosti s nimiz do jednotlivych skupin patri
+table(Iris[-train,"Sp"],predpovedi$class)
+# klasifikacni tabulka, jak dobre se trefil: v radcich skutecne hodnoty, ve sloupcich predikce
+plot(predpovedi$x[,1],predpovedi$x[,2],pch=19,col=predpovedi$class,
+     main="Graf diskriminacnich funkci",xlab="LD1",ylab="LD2")
+legend(9,2.2,legend=c(unique(predpovedi$class)),pch=19,col=1:3)
+# graf ukazujici kvalitu klasifikace
+################################
+## Shlukova analyza
+# budem delit americke staty do skupin na zaklade 4 ukazatelu: vrazdy, napadeni, populace, znasilneni
+# hierarchicke clusterovani
+hc <- hclust(dist(USArrests), "ave")
+# average linkage
+hc <- hclust(dist(USArrests))
+# complete linkage
+# vstupem je matice vzdalenosti jednotlivych bodu
+plot(hc, hang = -1)
+# nakresleni dendrogramu - postup, jak shlukuje
+#	nejprve ma kazde pozorovani svou vlastni skupinu, a ty se pak spojuji do vetsich celku
+#	mozny je i obraceny postup, tj. od jedne velke skupiny k mnoha malym
+seg<-cutree(hc,k=4)
+# rozdeli data do 4 skupin
+table(seg)
+rect.hclust(hc, k=4, border="red")
+# zobrazi skupiny do grafu
+tapply(USArrests$Murder,as.factor(seg),mean)
+# spocita prumery za jednotlive shluky
+
+# K-means clustering
+require(graphics)
+# prace s dvojrozmernymi daty
+x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
+colnames(x) <- c("x", "y")
+# vygenerovani dat
+(cl <- kmeans(x, 2))
+# rozdeleni dat do dvou segmentu (pocet segmentu vybiram podle 'potreby')
+# mohu zkusit vice seskupeni a porovnat je mezi sebou
+plot(x, col = cl$cluster,pch=19)
+# zakresleni dat rozdelenych do skupin
+points(cl$centers, col = 1:2, pch = 8, cex=2,lwd=2)
+# zakresleni stredu
